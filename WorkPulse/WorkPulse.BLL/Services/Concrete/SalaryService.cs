c4dbc8a161bc2b29f216a3a78a0a665f36e8f9ce
Aqsin21 <150579577+Aqsin21@users.noreply.github.com>
2025-11-16 21:17:41 +0400

    Salary Service Part 1

---
+++ WorkPulse/WorkPulse.BLL/Services/Concrete/SalaryService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkPulse.BLL.Services.Concrete
+{
+    public class SalaryService
+    {
+    }
+}
